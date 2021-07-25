3c810f5c43f432e514d879006095a9605c403e80
carlos alberto ferreira <58960107+Carlos-Carsdfj@users.noreply.github.com>
2021-07-24 21:45:48 -0400

    Update index.js

--- src/pages/userList/index.js
+++ src/pages/userList/index.js
@@ -37,7 +37,6 @@ export default function UserList() {
     getAlluser()
       .then(users=>{
         users && setUsers(users)
-        console.log(users)
         setChecking(false)
       })
   }, [])
